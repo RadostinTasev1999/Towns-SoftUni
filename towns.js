ba14a557f4fd1af474258dc4df8760aea7a4c2d1
RadostinTasev1999 <radostintasev22@yahoo.com>
2026-01-11 09:55:14 +0200

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+	const deleteBtn = document.getElementById("btnDelete")
+	deleteBtn.addEventListener("click",deleteTown)
+
+	const addButton = document.getElementById("btnAdd")
+	addButton.addEventListener("click",addTown)
 });
 
+/*
+$(document).ready(function() {...})
+
+- This is a jQuery method that ensures the enclosed code runs only after the DOM (Document Object Model) is fully loaded.
+  It prevents any code from executing before the HTML elements are available for manipulation.
+
+*/
+
 function deleteTown() {
+
+	console.log('DeleteTown invoked!')
 	let townName = $('#townName').val();
 	$('#townName').val('');
 	let removed = false;
@@ -16,4 +30,20 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+
+function addTown() {
+	const town = document.getElementById("addTown").value
+	console.log('Town is:', town)
+
+	if (town !== "") {
+		let option = document.createElement('option')
+		option.innerHTML = town
+		const selectEl = document.getElementById('towns')
+		selectEl.appendChild(option)
+		document.getElementById("addTown").value = ""
+		console.log(document.querySelector("div#result").innerHTML)
+		document.querySelector("div#result").innerHTML = `${town} has been successfully added!`
+	}
 }
