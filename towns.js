96c4efbcee5b7d1f810b91b4b4ae82a364f14b73
RadostinTasev1999 <radostintasev22@yahoo.com>
2026-01-12 20:10:28 +0200

    Implemented functionality to shuffle the existing towns

--- towns.js
+++ towns.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 
 	const addButton = document.getElementById("btnAdd")
 	addButton.addEventListener("click",addTown)
+
+	const shuffleBtn = document.getElementById("btnShuffle")
+	shuffleBtn.addEventListener("click",shuffleTown)
 });
 
 /*
@@ -47,3 +50,50 @@ function addTown() {
 		document.querySelector("div#result").innerHTML = `${town} has been successfully added!`
 	}
 }
+
+// Shuffler
+
+function shuffleTown() {
+	console.log('ShuffleTown invoked!')
+	let towns = document.querySelectorAll("#towns option")
+	let array = Array.from(towns)
+	let townArray = array.map((el) => el.innerHTML)
+
+	console.log('Towns are:', townArray)
+
+		function shuffleArray(townArray){
+			for (let i = townArray.length - 1; i > 0; i--) {
+				// i = 3
+				const j = Math.floor(Math.random() * (i+1));
+				// j = 2
+				const oldElement = townArray[i] //townArray[3] "Bourgas"
+				townArray[i] = townArray[j]
+				// townArray[3] = townArray[2]
+
+				townArray[j] = oldElement
+				// townArray[2] = townArray[3]
+				
+			}
+		}
+	
+	shuffleArray(townArray)
+	console.log('Shuffled towns are:', ...townArray)
+	
+	let selectList = document.getElementById('towns')
+	selectList.replaceChildren()
+	
+	townArray.forEach((town) => {
+		const option = document.createElement("option")
+		option.innerHTML = town
+		selectList.appendChild(option)
+	})
+
+	// ------------------------------------------------
+	const resultEl = document.getElementById("result")
+	resultEl.innerHTML = "Towns shuffled"
+	
+	setTimeout(() => {
+		resultEl.innerHTML = ""
+	},3000)
+}
+
